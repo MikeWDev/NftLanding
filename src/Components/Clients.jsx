8535fc3cba7df4e594659a1018455d3714e3f2a8
MikeWDev <130941015+MikeWDev@users.noreply.github.com>
2024-02-23 12:05:12 +0100

    Complete client and super rare sections

--- src/Components/Clients.jsx
+++ src/Components/Clients.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import clients1 from "../Assets/clients1.png";
+import clients2 from "../Assets/clients2.png";
+import clients3 from "../Assets/clients3.png";
+import clients4 from "../Assets/clients4.png";
+import clients5 from "../Assets/clients5.png";
 
 const Clients = () => {
-  return <div></div>;
+  const data = [clients1, clients2, clients3, clients4, clients5];
+  console.log(data);
+  return (
+    <div className="clients">
+      <div className="container">
+        <div className="clients-container">
+          {data.map((client, i) => {
+            return (
+              <div className="client" key={i}>
+                <img src={client} alt="client" />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Clients;
--- src/Components/FreeNft.jsx
+++ src/Components/FreeNft.jsx
@@ -1,7 +1,51 @@
 import React from "react";
-
+import icon from "../Assets/icon.png";
+import super1 from "../Assets/super1.png";
+import release2 from "../Assets/release2.png";
+import Card from "./Card";
 const FreeNft = () => {
-  return <div></div>;
+  return (
+    <div className="free">
+      <div className="container">
+        <div className="background">
+          <div className="ellipse pink"></div>
+          <div className="ellipse green"></div>
+
+          <div className="content">
+            <div className="image">
+              <img src={icon} alt="icon" />
+            </div>
+            <h2 className="title">Free NFT for early birds</h2>
+            <p className="description">
+              Sign up today you will get a free NFT when we launch
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="cards">
+        <div className="card1">
+          <Card
+            image={super1}
+            series={"Floop Series"}
+            title="Purple man"
+            price={2.99}
+            tag={12985}
+            time={1}
+          />
+        </div>
+        <div className="card2">
+          <Card
+            image={release2}
+            series={"Gloop Series"}
+            title="Purple man"
+            price={3.95}
+            tag={12985}
+            time={2}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FreeNft;
